fab2d9e146445cf417df0762b43d6700441c91d6
darlene9497 <nyamburadarlene6974@gmail.com>
2025-09-07 12:05:38 +0300

    :memo:complete week 1 assignment

---
+++ Week-1/index.js
@@ -0,0 +1,13 @@
+const num1 = 103;
+const num2 = 42;
+// const name = "Darlene";
+
+const name = "John";
+
+console.log(`Hello ${name}`);
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log("Hello " + name + ", the result of " + num1 + " + " + num2 + " is " + (num1 + num2));
